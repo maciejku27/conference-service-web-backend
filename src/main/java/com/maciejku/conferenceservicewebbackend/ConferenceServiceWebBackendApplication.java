65f46370e38bc98aefe574f239cd521b13857a2b
maciejku27 <kuczynski.maciej.nh@gmail.com>
2022-06-07 21:21:30 +0200

    .

---
+++ src/main/java/com/maciejku/conferenceservicewebbackend/ConferenceServiceWebBackendApplication.java
@@ -0,0 +1,13 @@
+package com.maciejku.conferenceservicewebbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConferenceServiceWebBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConferenceServiceWebBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/maciejku/conferenceservicewebbackend/ConferenceServiceWebBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.maciejku.conferenceservicewebbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConferenceServiceWebBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
